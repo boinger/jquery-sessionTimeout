5dc0507d826de8f4ee6f3b09f748f687047f14ea
jv <jeff@jeffvier.com>
2020-09-17 10:54:10 -0700

    new functionality merged in (manually)

--- jquery.sessionTimeout.js
+++ jquery.sessionTimeout.js
@@ -3,73 +3,81 @@
 //
 // jquery.sessionTimeout.js
 //
-// After a set amount of time, a dialog is shown to the user with the option
-// to either log out now, or stay connected. If log out now is selected,
-// the page is redirected to a logout URL. If stay connected is selected,
+// from https://github.com/boinger/jquery-sessionTimeout
+// based on https://github.com/travishorn/jquery-sessionTimeout/blob/master/jquery.sessionTimeout.js
+//
+// After a set amount of time, a dialog is shown to the user with the
+// option to either log out now, or stay connected. If log out now is
+// selected, a logout function is executed. If stay connected is selected,
 // a keep-alive URL is requested through AJAX. If no options is selected
-// after another set amount of time, the page is automatically redirected
-// to a timeout URL.
+// after another set amount of time, the page automatically executes a
+// timeout function.
 //
-//
-// USAGE
-//
-//   1. Include jQuery
-//   2. Include jQuery UI (for dialog)
-//   3. Include jquery.sessionTimeout.js
-//   4. Call $.sessionTimeout(); after document ready
+// As configured, the logout() function "tricks" the browser into terminating
+// a htaccess authenticated session using an invalid username/password
+// (logout/logout) to replace the current/valid ones.
 //
 //
 // OPTIONS
 //
 //   message
 //     Text shown to user in dialog after warning period.
-//     Default: 'Your session is about to expire.'
 //
 //   keepAliveUrl
 //     URL to call through AJAX to keep session alive. This resource should do something innocuous that would keep the session alive, which will depend on your server-side platform.
 //     Default: '/keep-alive'
 //
 //   keepAliveAjaxRequestType
-//     How should we make the call to the keep-alive url? (GET/POST/PUT)
+//     How should we make the call to the keepAliveUrl? (GET/POST/PUT)
 //     Default: 'POST'
 //
-//   redirUrl
-//     URL to take browser to if no action is take after warning period
-//     Default: '/timed-out'
+//   logoutClickAction
+//     Function to execute if user clicks "Log Out Now".  logout() accepts a parameter to append to the logoutUrl.
+//     Default: 'logout('1')'
+//
+//   timeoutAction
+//     Function to execute on timeout.  logout() accepts a parameter to append to the logoutUrl.
+//     Default: 'logout("timeout")'
+//
+//   preLogoutUrl
+//     URL to load with (intentionally) invalid username/password in order to invalidate an htaccess-authenticated session.  Must return a '200 OK'
+//     Default: /prelogout
 //
 //   logoutUrl
-//     URL to take browser to if user clicks "Log Out Now"
-//     Default: '/log-out'
+//     URL to load post logout.  logout() can append a string to this URL.
+//     Default: /logout?logoutreason=
 //
 //   warnAfter
 //     Time in milliseconds after page is opened until warning dialog is opened
 //     Default: 900000 (15 minutes)
 //
 //   redirAfter
-//     Time in milliseconds after page is opened until browser is redirected to redirUrl
+//     Time in milliseconds after page is opened until browser is logged out
 //     Default: 1200000 (20 minutes)
 //
 //   appendTime
-//     If true, appends the current time stamp to the Keep Alive url to prevent caching issues
+//     If true, appends the current time stamp to the keepAliveUrl to prevent caching issues
 //     Default: true
 //
-(function ($) {
-    jQuery.sessionTimeout = function (options) {
+(function($) {
+    jQuery.sessionTimeout = function(options) {
         var defaults = {
             message: 'Your session is about to expire.',
             keepAliveUrl: '/keep-alive',
             keepAliveAjaxRequestType: 'POST',
-            redirUrl: '/timed-out',
-            logoutUrl: '/log-out',
+            logoutClickAction: 'logout("clicked")',
+            timeoutAction: 'logout("timeout")',
+            preLogoutUrl: '/prelogout', // must return a '200 OK'
+            logoutUrl: '/logout?logoutreason=',
             warnAfter: 900000, // 15 minutes
             redirAfter: 1200000, // 20 minutes
-            appendTime: true // appends time stamp to keep alive url to prevent caching
+            appendTime: true // append time stamp to keepAliveUrl to prevent caching?
         };
 
         // Extend user-set options over defaults
         var o = defaults,
-				dialogTimer,
-				redirTimer;
+            dialogTimer,
+            redirTimer;
 
         if (options) { o = $.extend(defaults, options); }
 
@@ -80,14 +88,14 @@
             width: 400,
             modal: true,
             closeOnEscape: false,
-            open: function () { $(".ui-dialog-titlebar-close").hide(); },
+            open: function() { $(".ui-dialog-titlebar-close").hide(); },
             buttons: {
-                // Button one - takes user to logout URL
-                "Log Out Now": function () {
-                    window.location = o.logoutUrl;
+                // Button one - performs logoutAction
+                "Log Out Now": function() {
+                    eval(o.logoutClickAction);
                 },
-                // Button two - closes dialog and makes call to keep-alive URL
-                "Stay Connected": function () {
+                // Button two - closes dialog and makes call to keepAliveUrl
+                "Stay Connected": function() {
                     $(this).dialog('close');
 
                     $.ajax({
@@ -106,7 +114,7 @@
             switch (action) {
                 case 'start':
                     // After warning period, show dialog and start redirect timer
-                    dialogTimer = setTimeout(function () {
+                    dialogTimer = setTimeout(function() {
                         $('#sessionTimeout-dialog').dialog('open');
                         controlRedirTimer('start');
                     }, o.warnAfter);
@@ -122,8 +130,8 @@
             switch (action) {
                 case 'start':
                     // Dialog has been shown, if no action taken during redir period, redirect
-                    redirTimer = setTimeout(function () {
-                        window.location = o.redirUrl;
+                    redirTimer = setTimeout(function() {
+                        eval(o.timeoutAction);
                     }, o.redirAfter - o.warnAfter);
                     break;
 
@@ -153,7 +161,32 @@
             }
         }
 
-        $(document).ajaxComplete(function () {
+        function logout(value = '') {
+            var xmlhttp;
+            if (window.XMLHttpRequest) {
+                xmlhttp = new XMLHttpRequest();
+            }
+            // code for IE:
+            else if (window.ActiveXObject) {
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            if (window.ActiveXObject) {
+                // IE clear HTTP Authentication:
+                document.execCommand("ClearAuthenticationCache");
+                window.location.href = o.logoutUrl + value;
+            } else {
+                xmlhttp.open("GET", o.preLogoutUrl, true, "logout", "logout");
+                xmlhttp.send("");
+                xmlhttp.onreadystatechange = function() {
+                    if (xmlhttp.readyState == 4) { window.location.href = o.logoutUrl + value; }
+                }
+            }
+
+            return false;
+        }
+
+
+        $(document).ajaxComplete(function() {
             if (!$('#sessionTimeout-dialog').dialog("isOpen")) {
                 controlRedirTimer('stop');
                 controlDialogTimer('stop');
